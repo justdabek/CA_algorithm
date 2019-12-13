c82def5487884e20ae61598223847f3305628e3e
justdabek <jdabek13@gmail.com>
2019-12-13 19:24:21 +0100

    Lab 1 and 2 finished

--- Board.py
+++ Board.py
@@ -1,9 +1,11 @@
 from Cell import *
 import random
-import time
-import csv
-from numpy import array
+import math
+from csv import reader,writer,excel
+from numpy import array, loadtxt
 from PIL import Image
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 
 MOORE_NEIGHBOURHOOD = [(-1,-1), (0,-1),  (+1,-1),
                        (-1,0),(0,0),  (+1,0),
@@ -23,23 +25,31 @@ PENTAGONAL_RIGHT = [ (0,-1),  (+1,-1),
 
 colour_rule = {
     # Syntax is - cell value: (R, G, B)
-    0: (255, 255, 255)
+    0: (255, 255, 255),
+    1: (0,0,0)
     }
 
 def generateColorRule(nrOfColours):
-    for key in range(1,nrOfColours+1):
+    for key in range(2,nrOfColours+1):
         colour_rule[key]=(random.randrange(0,255,1,int),random.randrange(0,255,1,int),random.randrange(0,255,1,int))
 
 
 class Board(Cell):
-    def __init__(self,dimX,dimY,nrOfSeeds,neighborhood):
+    def __init__(self,dimX,dimY,nrOfSeeds,neighborhood, border, radius,nrOfInclusions):
         self.dimX=dimX
         self.dimY=dimY
         self.board=[[]]
+        self.neighborhood=neighborhood
+        self.border=border
+        self.minR=radius[0]
+        self.maxR=radius[1]
+        self.nrOfInclusions=nrOfInclusions
+
         self.generate()
         self.generateSeeds(nrOfSeeds)
-        self.neighborhood=neighborhood
-        self.boarder=False
+        if(self.nrOfInclusions>0):
+            if(radius[0]!=0 and radius[1]!=0):
+                self.generateInclusions()
 
 
     def getCell(self,row,col):
@@ -53,24 +63,37 @@ class Board(Cell):
         self.board[row][col].setColour(colour_rule[ID])
 
     def generate(self):
-        self.board=[[Cell(i,j,0,0,colour_rule[0]) for i in range(0,self.dimX)] for j in range(0,self.dimY)]
+        self.board=[[Cell(i,j,0,0,colour_rule[0]) for j in range(0,self.dimY)] for i in range(0,self.dimX)]
 
+    def generateInclusions(self):
+        for inclusion in range(0,self.nrOfInclusions):
+            r = random.randrange(self.minR,self.maxR,1,int)
+            cx = random.randrange(0, self.dimX, 1, int)
+            cy = random.randrange(0, self.dimY, 1, int)
+            for x in range(cx - r, (cx + r)+1):
+                for y in range(cy - r, (cy + r)+1):
+                    if x < 0 or x >= self.dimX or y < 0 or y >= self.dimY:
+                        x = x % self.dimX
+                        y = y % self.dimY
+                    if math.sqrt((cx - x) ** 2 + (cy - y) ** 2) <= r:
+                        self.setCell(x,y,1,1,colour_rule[1])
 
     def generateSeeds(self,nrOfSeeds):
         generateColorRule(nrOfSeeds)
-        for s in range(1,nrOfSeeds+1):
+        for s in range(2,nrOfSeeds+1):
             row=random.randrange(0,self.dimX,1,int)
             col=random.randrange(0,self.dimY,1,int)
             ID=s
             colour=colour_rule[ID]
-            self.setCell(row,col,ID,ID,colour)
+            if(self.getCell(row,col).ID==0):
+                self.setCell(row,col,ID,ID,colour)
 
 
     def iteration(self):
         empty_cell=0
         for i in range(0,self.dimX):
             for j in range(0,self.dimY):
-                if(self.getCell(i,j).getID()):
+                if(self.getCell(i,j).getID() and self.getCell(i,j).getID()!=1):
                     self.evolveCell(i,j)
                 else:
                     empty_cell+=1
@@ -90,7 +113,7 @@ class Board(Cell):
             nei_row = (row + rule[0])
             nei_col = (col + rule[1])
 
-            if self.boarder is True:
+            if self.border is True:
                 if nei_row < 0 or nei_row >= self.dimX or nei_col < 0 or nei_col >= self.dimY:
                     IDList.append(0)
                     continue
@@ -99,7 +122,9 @@ class Board(Cell):
                     nei_row = nei_row % self.dimX
                     nei_col = nei_col % self.dimY
 
-            IDList.append(self.board[nei_row][nei_col].ID)
+            if(self.board[nei_row][nei_col].ID!=1):
+                IDList.append(self.board[nei_row][nei_col].ID)
+
 
         ID = self.calculateID(IDList,self.getCell(row,col).getID())
         return ID
@@ -109,7 +134,7 @@ class Board(Cell):
             nei_row = (row + rule[0])
             nei_col = (col + rule[1])
 
-            if self.boarder is True:
+            if self.border is True:
                 if nei_row < 0 or nei_row >= self.dimX or nei_col < 0 or nei_col >= self.dimY:
                    continue
             else:
@@ -140,27 +165,47 @@ class Board(Cell):
         ID=self.getNeighbors(row,col)
         self.setNeighbors(row,col,ID)
 
-    def intBoard(self):
-        intBoard = array([[self.board[i][j].colour for i in range(0, self.dimX)] for j in range(0, self.dimY)])
-        return intBoard
-
     def intBoardID(self):
-        intBoard = array([[self.board[i][j].ID for i in range(0, self.dimX)] for j in range(0, self.dimY)])
+        intBoard = array([[self.board[i][j].ID for j in range(0, self.dimY)] for i in range(0, self.dimX)])
         return intBoard
 
     def saveImage(self):
-        img = Image.new('RGB', (self.dimX, self.dimX))
+        img = Image.new('RGB', (self.dimX, self.dimX), "white")
         pixels = img.load()
 
-        for j in range(0, self.dimX):
-            for i in range(0, self.dimY):
+        for i in range(0, self.dimX):
+            for j in range(0, self.dimY):
                 pixels[i, j] = colour_rule[self.board[i][j].getID()]
         img.save('CA_img.png')
         return img
 
     def writeToCSV(self):
-        with open("shot.csv", "a") as my_csv:
+        Tk().withdraw()
+        name = asksaveasfilename(title='shot', defaultextension='.csv')
+        with open(name, "w") as my_csv:
             my_csv.seek(0)
-            csvWriter = csv.writer(my_csv, delimiter=',')
+            csvWriter = writer(my_csv)
             arr=self.intBoardID()
             csvWriter.writerows(arr)
+
+    def importFromCSV(self):
+        Tk().withdraw()
+        filename = askopenfilename()
+        print("filename:", filename)
+        with open(filename, mode='r') as csv_file:
+            csv_reader = reader(csv_file, dialect=excel)
+            result=[]
+            for line in csv_reader:
+                if len(line)>0:
+                    result.append(line)
+            print(result)
+        self.dimX=len(result[0])
+        self.dimY=len(result)
+
+        for i in range(0, self.dimX):
+            for j in range(0, self.dimY):
+                ID=int(result[i][j])
+                self.board[i][j]=Cell(i,j,ID,ID,colour_rule[ID])
+
+
+
--- CellularAutomata.py
+++ CellularAutomata.py
@@ -1,5 +1,3 @@
-from Board import *
-
 # -*- coding: utf-8 -*-
 
 # Form implementation generated from reading ui file 'CA_GUI.ui'
@@ -14,28 +12,20 @@ from PyQt5.QtCore import QTimer
 from Image import *
 from Board import *
 
-import time
-
-SIZE_WIDTH=100
-SIZE_HEIGHT=100
-SEEDS=200
-
-
-
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(557, 719)
+        MainWindow.resize(557, 707)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
         self.Image = QtWidgets.QLabel(self.centralwidget)
-        self.Image.setGeometry(QtCore.QRect(20, 20, 400, 400))
+        self.Image.setGeometry(QtCore.QRect(50, 10, 431, 431))
         self.Image.setText("")
         self.Image.setPixmap(QtGui.QPixmap("CA_img.png"))
         self.Image.setScaledContents(True)
-        self.Image.setAlignment(QtCore.Qt.AlignCenter)
+        self.Image.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
         self.Image.setWordWrap(False)
         self.Image.setObjectName("Image")
 
@@ -50,7 +40,6 @@ class Ui_MainWindow(object):
         self.Play.setObjectName("Play")
         self.horizontalLayout.addWidget(self.Play)
         self.Play.setCheckable(True)
-        self.Play.toggle()
         self.Play.clicked.connect(self.play_pause)
 
         self.Next = QtWidgets.QPushButton(self.horizontalLayoutWidget)
@@ -64,59 +53,67 @@ class Ui_MainWindow(object):
         self.Reset.clicked.connect(self.reset)
 
         self.Dimensions = QtWidgets.QFrame(self.centralwidget)
-        self.Dimensions.setGeometry(QtCore.QRect(20, 510, 111, 101))
+        self.Dimensions.setGeometry(QtCore.QRect(20, 510, 111, 91))
         self.Dimensions.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.Dimensions.setFrameShadow(QtWidgets.QFrame.Raised)
         self.Dimensions.setObjectName("Dimensions")
 
         self.X_edit = QtWidgets.QLineEdit(self.Dimensions)
-        self.X_edit.setGeometry(QtCore.QRect(20, 40, 51, 20))
+        self.X_edit.setGeometry(QtCore.QRect(20, 20, 51, 20))
         self.X_edit.setObjectName("X_edit")
-        self.X_edit.setText('5')
+        self.X_edit.setText('200')
 
         self.Y_edit = QtWidgets.QLineEdit(self.Dimensions)
-        self.Y_edit.setGeometry(QtCore.QRect(20, 70, 51, 20))
+        self.Y_edit.setGeometry(QtCore.QRect(20, 50, 51, 20))
         self.Y_edit.setObjectName("Y_edit")
-        self.Y_edit.setText('5')
+        self.Y_edit.setText('200')
 
         self.X = QtWidgets.QLabel(self.Dimensions)
-        self.X.setGeometry(QtCore.QRect(10, 40, 16, 16))
+        self.X.setGeometry(QtCore.QRect(10, 20, 16, 16))
         self.X.setObjectName("X")
-        self.X_2 = QtWidgets.QLabel(self.Dimensions)
-        self.X_2.setGeometry(QtCore.QRect(10, 70, 16, 16))
-        self.X_2.setObjectName("X_2")
+        self.Y = QtWidgets.QLabel(self.Dimensions)
+        self.Y.setGeometry(QtCore.QRect(10, 50, 16, 16))
+        self.Y.setObjectName("Y")
 
         self.Dimensions_2 = QtWidgets.QLabel(self.Dimensions)
-        self.Dimensions_2.setGeometry(QtCore.QRect(10, 10, 61, 16))
+        self.Dimensions_2.setGeometry(QtCore.QRect(10, 0, 61, 16))
         self.Dimensions_2.setObjectName("Dimensions_2")
 
         self.frame = QtWidgets.QFrame(self.centralwidget)
-        self.frame.setGeometry(QtCore.QRect(20, 610, 111, 61))
+        self.frame.setGeometry(QtCore.QRect(20, 590, 111, 91))
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame.setObjectName("frame")
 
-        self.label = QtWidgets.QLabel(self.frame)
-        self.label.setGeometry(QtCore.QRect(10, 10, 101, 16))
-        self.label.setObjectName("label")
+        self.NrOfSeeds = QtWidgets.QLabel(self.frame)
+        self.NrOfSeeds.setGeometry(QtCore.QRect(10, 0, 101, 16))
+        self.NrOfSeeds.setObjectName("NrOfSeeds")
 
         self.Seeds_edit = QtWidgets.QLineEdit(self.frame)
-        self.Seeds_edit.setGeometry(QtCore.QRect(10, 30, 51, 20))
+        self.Seeds_edit.setGeometry(QtCore.QRect(10, 20, 51, 20))
         self.Seeds_edit.setObjectName("Seeds_edit")
-        self.Seeds_edit.setText('3')
+        self.Seeds_edit.setText('10')
+
+        self.NrOfInclusions = QtWidgets.QLabel(self.frame)
+        self.NrOfInclusions.setGeometry(QtCore.QRect(10, 40, 101, 16))
+        self.NrOfInclusions.setObjectName("NrOfInclusions")
+
+        self.Inclusions_edit = QtWidgets.QLineEdit(self.frame)
+        self.Inclusions_edit.setGeometry(QtCore.QRect(10, 60, 51, 20))
+        self.Inclusions_edit.setObjectName("Inclusions_edit")
 
         self.frame_2 = QtWidgets.QFrame(self.centralwidget)
-        self.frame_2.setGeometry(QtCore.QRect(130, 510, 181, 161))
+        self.frame_2.setGeometry(QtCore.QRect(130, 500, 181, 181))
         self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame_2.setObjectName("frame_2")
 
         self.Method = QtWidgets.QLabel(self.frame_2)
-        self.Method.setGeometry(QtCore.QRect(10, 10, 47, 13))
+        self.Method.setGeometry(QtCore.QRect(10, 0, 47, 13))
         self.Method.setObjectName("Method")
 
         self.comboBox = QtWidgets.QComboBox(self.frame_2)
-        self.comboBox.setGeometry(QtCore.QRect(10, 40, 151, 22))
+        self.comboBox.setGeometry(QtCore.QRect(10, 20, 151, 22))
         self.comboBox.setObjectName("comboBox")
         self.comboBox.addItem("Moore")
         self.comboBox.addItem("Von Neumann")
@@ -125,27 +122,36 @@ class Ui_MainWindow(object):
         self.comboBox.addItem("Pentagonal left")
         self.comboBox.addItem("Pentagonal right")
 
+        self.radioButton_Torus = QtWidgets.QRadioButton(self.frame_2)
+        self.radioButton_Torus.setGeometry(QtCore.QRect(10, 60, 82, 17))
+        self.radioButton_Torus.setObjectName("radioButton")
+        self.radioButton_Torus.setChecked(True)
+        self.radioButton_Border = QtWidgets.QRadioButton(self.frame_2)
+        self.radioButton_Border.setGeometry(QtCore.QRect(70, 60, 82, 17))
+        self.radioButton_Border.setObjectName("radioButton_2")
+
         self.Inclusions = QtWidgets.QLabel(self.frame_2)
-        self.Inclusions.setGeometry(QtCore.QRect(10, 80, 61, 16))
-        self.Inclusions.setObjectName("Inclusions")
+        self.Inclusions.setGeometry(QtCore.QRect(10, 90, 91, 16))
+        self.Inclusions.setObjectName("Inclusions - radius")
 
         self.Min_edit = QtWidgets.QLineEdit(self.frame_2)
-        self.Min_edit.setGeometry(QtCore.QRect(40, 100, 51, 20))
+        self.Min_edit.setGeometry(QtCore.QRect(50, 110, 51, 20))
         self.Min_edit.setObjectName("Min_edit")
 
         self.Max_edit = QtWidgets.QLineEdit(self.frame_2)
-        self.Max_edit.setGeometry(QtCore.QRect(40, 130, 51, 20))
+        self.Max_edit.setGeometry(QtCore.QRect(50, 150, 51, 20))
         self.Max_edit.setObjectName("Max_edit")
 
         self.Min = QtWidgets.QLabel(self.frame_2)
-        self.Min.setGeometry(QtCore.QRect(10, 100, 31, 16))
+        self.Min.setGeometry(QtCore.QRect(10, 110, 31, 16))
         self.Min.setObjectName("Min")
+
         self.Max = QtWidgets.QLabel(self.frame_2)
-        self.Max.setGeometry(QtCore.QRect(10, 130, 31, 16))
+        self.Max.setGeometry(QtCore.QRect(10, 150, 31, 16))
         self.Max.setObjectName("Max")
 
         self.Generate = QtWidgets.QPushButton(self.centralwidget)
-        self.Generate.setGeometry(QtCore.QRect(330, 640, 191, 31))
+        self.Generate.setGeometry(QtCore.QRect(320, 620, 201, 51))
         self.Generate.setObjectName("Generate")
         self.Generate.clicked.connect(self.generate)
 
@@ -157,13 +163,14 @@ class Ui_MainWindow(object):
         self.frame_3.setObjectName("frame_3")
 
         self.Import = QtWidgets.QPushButton(self.frame_3)
-        self.Import.setGeometry(QtCore.QRect(30, 20, 75, 23))
+        self.Import.setGeometry(QtCore.QRect(10, 10, 91, 23))
         self.Import.setObjectName("Import")
+        self.Import.clicked.connect(self.importCSV)
 
         self.Export = QtWidgets.QPushButton(self.frame_3)
-        self.Export.setGeometry(QtCore.QRect(120, 20, 75, 23))
+        self.Export.setGeometry(QtCore.QRect(110, 10, 91, 23))
         self.Export.setObjectName("Export")
-        self.Export.clicked.connect(self.export)
+        self.Export.clicked.connect(self.exportCSV)
 
         self.timer=QTimer()
         self.timer.setInterval(100)
@@ -178,18 +185,25 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-        self.board=Board(200,200,20,MOORE_NEIGHBOURHOOD)
+        self.board=Board(200,200,20,MOORE_NEIGHBOURHOOD,True,(0,0),0)
 
     def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.Play.setText(_translate("MainWindow", "Play/Pause"))
         self.Next.setText(_translate("MainWindow", "Next"))
         self.Reset.setText(_translate("MainWindow", "Reset"))
         self.X.setText(_translate("MainWindow", "X"))
-        self.X_2.setText(_translate("MainWindow", "Y"))
+        self.X_edit.setText(_translate("MainWindow","200"))
+        self.Y.setText(_translate("MainWindow", "Y"))
+        self.Y_edit.setText(_translate("MainWindow","200"))
         self.Dimensions_2.setText(_translate("MainWindow", "Dimensions"))
-        self.label.setText(_translate("MainWindow", "Number of seeds"))
+        self.NrOfSeeds.setText(_translate("MainWindow", "Number of seeds"))
+        self.Seeds_edit.setText(_translate("MainWindow","100"))
+        self.NrOfInclusions.setText(_translate("MainWindow", "Number of inclusions"))
+        self.Inclusions_edit.setText(_translate("MainWindow","20"))
         self.Method.setText(_translate("MainWindow", "Method"))
         self.comboBox.setItemText(0, _translate("MainWindow", "MOORE"))
         self.comboBox.setItemText(1, _translate("MainWindow", "VON NEUMANN"))
@@ -197,9 +211,14 @@ class Ui_MainWindow(object):
         self.comboBox.setItemText(3, _translate("MainWindow", "HEXAGONAL RIGHT"))
         self.comboBox.setItemText(4, _translate("MainWindow", "PENTAGONAL LEFT"))
         self.comboBox.setItemText(5, _translate("MainWindow", "PENTAGONAL RIGHT"))
-        self.Inclusions.setText(_translate("MainWindow", "Inclusions"))
+
+        self.Inclusions.setText(_translate("MainWindow", "Inclusions - radius"))
+        self.Min_edit.setText(_translate("MainWindow", "1"))
+        self.Max_edit.setText(_translate("MainWindow", "10"))
         self.Min.setText(_translate("MainWindow", "Min"))
         self.Max.setText(_translate("MainWindow", "Max"))
+        self.radioButton_Torus.setText(_translate("MainWindow", "Torus"))
+        self.radioButton_Border.setText(_translate("MainWindow", "Boundry"))
         self.Generate.setText(_translate("MainWindow", "Generate"))
         self.Import.setText(_translate("MainWindow", "Import"))
         self.Export.setText(_translate("MainWindow", "Export"))
@@ -251,12 +270,24 @@ class Ui_MainWindow(object):
         X=self.X_edit.text()
         Y=self.Y_edit.text()
         Neighborhood=self.combo_choose()
-        self.board=Board(int(X),int(Y),int(nrOfSeeds),Neighborhood)
+        min_r_incl=self.Min_edit.text()
+        max_r_incl=self.Max_edit.text()
+        nrOfInclusions=self.Inclusions_edit.text()
+        if(self.radioButton_Border.isChecked()):
+            border=True
+        else:
+            border=False
+
+        self.board=Board(int(X),int(Y),int(nrOfSeeds),Neighborhood,border,(int(min_r_incl),int(max_r_incl)),int(nrOfInclusions))
         self.refresh()
         # self.reset()
 
-    def export(self):
+    def exportCSV(self):
         self.board.writeToCSV()
+
+    def importCSV(self):
+        self.board.importFromCSV()
+        self.refresh()
 
     def refresh(self):
         self.board.updateBoard()
